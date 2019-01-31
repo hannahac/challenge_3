782b43c21d9d136999f8befdc9d228bd8f3cd4bc
hannahac <hannahapio@gmail.com>
2019-01-31 15:44:09 +0300

    refactor code

--- listsort.py
+++ listsort.py
@@ -1,17 +1,17 @@
 class numbers():
-                
-        def __return_in_init__(self, mynumbers):
+        def __init__(self, mynumbers):
                 self.mynumbers = mynumbers
-                mynumbers = [1, 2, 3, 4, 5, 6, 7, 8, 'a', 'b', 'c', 'd', 'e', 'f']
-                if __'mynumbers'__ == __main__ :
-                        if mynumbers % 2 == 0:
-                                return 'evens'
-                        elif mynumbers % 2 >= 1:
-                                return 'odds' 
-                        elif mynumbers == chr:
-                                return 'chrs'
+        def myfunc(self):
+                x = sorted(mynumbers)       
+                print(f"{1}: {x}")
+                if mynumbers % 2 == 0:
+                        return 'evens'
+                elif mynumbers % 2 >= 1:
+                        return 'odds' 
+                elif mynumbers == chr:
+                        return 'chrs'
 
-
+mynumbers = [1, 2, 3, 4, 5, 6, 7, 8, 'a', 'b', 'c', 'd', 'e', 'f']
 print(numbers)                
     
     
