a4676d1a542964c75b1314e935590cf88dba5110
hannahac <hannahapio@gmail.com>
2019-01-31 14:58:42 +0300

    refactor code

--- listsort.py
+++ listsort.py
@@ -1,16 +1,20 @@
-# def list(lists):
-lists = [1, 2, 3, 4, 5, 6, 7, 8, 'a', 'b',
-        'c', 'd', 'e', 'f', 'g', 'h']
-evens = []
-odds = []
-# chars = []
-for value in lists:
-    if value % 2 == 0:
-                evens.append(value)
-    else: 
-                odds.append(value)
-    # else:
-    #             chars.append('others')        
-print("evens: ", evens )
-print("odds: ", odds)
-# print("chars: ", chars)
+class numbers():
+                
+        def __return_in_init__(self, mynumbers):
+                self.mynumbers = mynumbers
+                mynumbers = [1, 2, 3, 4, 5, 6, 7, 8, 'a', 'b', 'c', 'd', 'e', 'f']
+                if __'mynumbers'__ == __main__ :
+                        if mynumbers % 2 == 0:
+                                return 'evens'
+                        elif mynumbers % 2 >= 1:
+                                return 'odds' 
+                        elif mynumbers == chr:
+                                return 'chrs'
+
+
+print(numbers)                
+    
+    
+                
+               
+                
