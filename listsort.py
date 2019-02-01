5888e0ce6c31df517826e262004b507f1aa933aa
hannahac <hannahapio@gmail.com>
2019-02-01 14:29:25 +0300

    add changes to file

--- listsort.py
+++ listsort.py
@@ -2,8 +2,8 @@ class numbers():
         def __init__(self, mynumbers):
                 self.mynumbers = mynumbers
         def myfunc(self):
-                x = sorted(mynumbers)       
-                print(f"{1}: {x}")
+                x = sorted(mynumbers)
+                print(x)       
                 if mynumbers % 2 == 0:
                         return 'evens'
                 elif mynumbers % 2 >= 1:
